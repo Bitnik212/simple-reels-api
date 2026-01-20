47cf5351440183b5c8b4679bdffd343e6ab91b03
Никоноров Николай Витальевич <nnv@bitt.moe>
2026-01-21 01:06:02 +0300

    Configure HTTP headers and OpenAPI/Swagger in 'configureHTTP'

---
+++ src/main/kotlin/HTTP.kt
@@ -0,0 +1,33 @@
+package moe.bitt
+
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.callid.*
+import io.ktor.server.plugins.calllogging.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.defaultheaders.*
+import io.ktor.server.plugins.openapi.*
+import io.ktor.server.plugins.swagger.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import java.sql.Connection
+import java.sql.DriverManager
+import org.jetbrains.exposed.sql.*
+import org.koin.dsl.module
+import org.koin.ktor.plugin.Koin
+import org.koin.logger.slf4jLogger
+import org.slf4j.event.*
+
+fun Application.configureHTTP() {
+    install(DefaultHeaders) {
+        header("X-Engine", "Ktor") // will send this header with each response
+    }
+    routing {
+        openAPI(path = "openapi")
+    }
+    routing {
+        swaggerUI(path = "openapi")
+    }
+}
