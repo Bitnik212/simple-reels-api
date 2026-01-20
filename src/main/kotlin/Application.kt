09863692fc2de8c1a66f6f9171cd5376822a1176
Никоноров Николай Витальевич <nnv@bitt.moe>
2026-01-21 02:21:26 +0300

    Add utility functions for JSON responses, proxy handling, and header parsing in `utils.kt`

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,8 +1,12 @@
 package moe.bitt
 
 import io.ktor.server.application.*
+import io.ktor.server.config.ApplicationConfig
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import org.koin.dsl.module
+import org.koin.ktor.plugin.Koin
+import org.koin.logger.slf4jLogger
 
 
 fun main() {
@@ -10,10 +14,22 @@ fun main() {
 }
 
 fun Application.module() {
+    val config = ApplicationConfig("application.yaml")
+
+    val initModule = module {
+        single<ApplicationConfig> {
+            config
+        }
+        single<ApplicationEnvironment> {
+            environment
+        }
+    }
+
+    install(Koin) {
+        slf4jLogger()
+        modules(mainModule, initModule)
+    }
     configureHTTP()
     configureMonitoring()
-    configureSerialization()
-//    configureDatabases()
-    configureFrameworks()
     configureRouting()
 }
